2acb6563114cfb10366fb989de9725b878b210a5
Andrew Coleburn <arcoleburn@gmail.com>
2021-03-05 18:27:11 -0500

    cleaning code

--- src/server.js
+++ src/server.js
@@ -1,11 +1,10 @@
 'use strict';
 const app = require('./app');
 
-const { PORT, DATABASE_URL } = require('./config');
+const { PORT } = require('./config');
 const NODE_ENV = process.env.NODE_ENV;
 
 
-app.set('db', db);
 app.listen(PORT, () => {
   console.log(`Server listening in ${NODE_ENV}`);
 });
